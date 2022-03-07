b64bbfe123ae55d4c6c7251c64ddc08c8e5effe6
CaduRoriz <ceduardo.roriz@gmail.com>
2022-03-07 13:40:53 -0300

    adicionado pequena alteracao do style dos cards no hover

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 var audioPlayer = document.getElementById('audioplayer');
 var loaded = false;
 var playBtn = document.getElementById('playBtn');
@@ -10,33 +9,40 @@ pauseBtn.addEventListener('click', (event) => {  /**/
     
     playBtn.style.display = "inline";                                       /*  aqui eu faço com que troque os simbolos assim que eu clickar em um deles*/
     pauseBtn.style.display= "none";
-    
+    audioPlayer.pause();
     
     return false;
 });
 
+playBtn.addEventListener('click', (event) => {  /**/
+    event.preventDefault();
+    
+    playBtn.style.display = "none";                                       /*  aqui eu faço com que troque os simbolos assim que eu clickar em um deles*/
+    pauseBtn.style.display= "inline";
+    audioPlayer.play();
+    
+    return false;
+});
 
 function playSong(file){
-
+    
     if(loaded == false){   /*se for false quer dizer que eh a primeira vez que estou carregando meu arquivo*/
         audioPlayer.innerHTML = `<source src ="`+file+`" type="audio/mp3" />`;
         loaded = true;
     }
-
     audioPlayer.play();
     playBtn.style.display = "none";                                       /*  aqui eu faço com que troque os simbolos assim que eu clickar em um deles*/
     pauseBtn.style.display= "inline";
+    
 }
+
 
 
 
 let cards = document.querySelectorAll('.main__col');
 
-console.log(cards);
-
 cards.forEach(card => { 
     card.addEventListener('click', ()=>{ 
-        alert('clicado')
         let image = card.getAttribute('data-image');
         let artist = card.getAttribute('data-artist');            let song = card.getAttribute('data-song');
         let file= card.getAttribute('data-file');
@@ -49,3 +55,19 @@ cards.forEach(card => {
             
         })
     })
+
+
+const barraProgresso = document.querySelector('.player__control__progress2');
+    
+setInterval(atualizaBarra, 50); /*essa funcao vai chamar a atualizaBarra a cada meio segundo(500)*/ 
+
+function atualizaBarra () {
+    if(!audioPlayer.paused){
+
+        var progressoAudio = (audioPlayer.currentTime/audioPlayer.duration) * 100;
+
+        barraProgresso.style.width = `${progressoAudio}%`
+    }
+}
+
+alert("Amigos o site ta bem simplório, só implementei as funcionalidades relacionadas as músicas... então apesar de ter 'buscar' 'bioblioteca' 'criar playlist' e 'musiscas curtidas' esses links não fazem nada ");
